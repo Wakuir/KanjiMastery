54377d720f76d730d1227addd7fcc482bafeb0e0
Quentin BOGIRAUD <quentin.bogiraud@etu.univ-grenoble-alpes.fr>
2025-08-19 15:20:54 +0200

    Changed 'complieSdk' and 'targetSdk' level to 36 to work with latest dependency versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "fr.wakuir.kanjimastery"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "fr.wakuir.kanjimastery"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
