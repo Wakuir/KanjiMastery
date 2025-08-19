1e6ee7a68028b4ad7b76ffa6a82d1f99a33a5142
Quentin BOGIRAUD <quentin.bogiraud@etu.univ-grenoble-alpes.fr>
2025-08-19 12:08:55 +0200

    Changed the theme to a blue one

--- app/src/main/java/fr/wakuir/kanjimastery/ui/theme/Theme.kt
+++ app/src/main/java/fr/wakuir/kanjimastery/ui/theme/Theme.kt
@@ -8,82 +8,82 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
-private val LightColorScheme = lightColorScheme(
-    background = pure_white,
-
-    primary = light_theme_primary,
-    onPrimary = light_theme_on_primary,
-    secondary = light_theme_secondary,
-    onSecondary = light_theme_on_secondary,
-    tertiary = light_theme_tertiary,
-    onTertiary = light_theme_on_tertiary,
-    primaryContainer = light_theme_primary_container,
-    onPrimaryContainer = light_theme_on_primary_container,
-    secondaryContainer = light_theme_secondary_container,
-    onSecondaryContainer = light_theme_on_secondary_container,
-    tertiaryContainer = light_theme_tertiary_container,
-    onTertiaryContainer = light_theme_on_tertiary_container,
-    error = light_theme_error,
-    onError = light_theme_on_error,
-    errorContainer = light_theme_error_container,
-    onErrorContainer = light_theme_on_error_container,
-    surface = light_theme_surface,
-    surfaceDim = light_theme_surface_dim,
-    surfaceBright = light_theme_surface_bright,
-    onSurface = light_theme_on_surface,
-    onSurfaceVariant = light_theme_on_surface_variant,
-    outline = light_theme_outline,
-    outlineVariant = light_theme_outline_variant,
-    surfaceContainerLowest = light_theme_surface_container_lowest,
-    surfaceContainerLow = light_theme_surface_container_low,
-    surfaceContainer = light_theme_surface_container,
-    surfaceContainerHigh = light_theme_surface_container_high,
-    surfaceContainerHighest = light_theme_surface_container_highest,
-    inversePrimary = light_theme_inverse_primary,
-    inverseSurface = light_theme_inverse_surface,
-    inverseOnSurface = light_theme_inverse_on_surface,
-    scrim = light_theme_scrim,
-
+private val lightScheme = lightColorScheme(
+    primary = primaryLight,
+    onPrimary = onPrimaryLight,
+    primaryContainer = primaryContainerLight,
+    onPrimaryContainer = onPrimaryContainerLight,
+    secondary = secondaryLight,
+    onSecondary = onSecondaryLight,
+    secondaryContainer = secondaryContainerLight,
+    onSecondaryContainer = onSecondaryContainerLight,
+    tertiary = tertiaryLight,
+    onTertiary = onTertiaryLight,
+    tertiaryContainer = tertiaryContainerLight,
+    onTertiaryContainer = onTertiaryContainerLight,
+    error = errorLight,
+    onError = onErrorLight,
+    errorContainer = errorContainerLight,
+    onErrorContainer = onErrorContainerLight,
+    background = backgroundLight,
+    onBackground = onBackgroundLight,
+    surface = surfaceLight,
+    onSurface = onSurfaceLight,
+    surfaceVariant = surfaceVariantLight,
+    onSurfaceVariant = onSurfaceVariantLight,
+    outline = outlineLight,
+    outlineVariant = outlineVariantLight,
+    scrim = scrimLight,
+    inverseSurface = inverseSurfaceLight,
+    inverseOnSurface = inverseOnSurfaceLight,
+    inversePrimary = inversePrimaryLight,
+    surfaceDim = surfaceDimLight,
+    surfaceBright = surfaceBrightLight,
+    surfaceContainerLowest = surfaceContainerLowestLight,
+    surfaceContainerLow = surfaceContainerLowLight,
+    surfaceContainer = surfaceContainerLight,
+    surfaceContainerHigh = surfaceContainerHighLight,
+    surfaceContainerHighest = surfaceContainerHighestLight,
 )
 
-private val DarkColorScheme = darkColorScheme(
-    background = pure_black,
-
-    primary = dark_theme_primary,
-    onPrimary = dark_theme_on_primary,
-    secondary = dark_theme_secondary,
-    onSecondary = dark_theme_on_secondary,
-    tertiary = dark_theme_tertiary,
-    onTertiary = dark_theme_on_tertiary,
-    primaryContainer = dark_theme_primary_container,
-    onPrimaryContainer = dark_theme_on_primary_container,
-    secondaryContainer = dark_theme_secondary_container,
-    onSecondaryContainer = dark_theme_on_secondary_container,
-    tertiaryContainer = dark_theme_tertiary_container,
-    onTertiaryContainer = dark_theme_on_tertiary_container,
-    error = dark_theme_error,
-    onError = dark_theme_on_error,
-    errorContainer = dark_theme_error_container,
-    onErrorContainer = dark_theme_on_error_container,
-    surface = dark_theme_surface,
-    surfaceDim = dark_theme_surface_dim,
-    surfaceBright = dark_theme_surface_bright,
-    onSurface = dark_theme_on_surface,
-    onSurfaceVariant = dark_theme_on_surface_variant,
-    outline = dark_theme_outline,
-    outlineVariant = dark_theme_outline_variant,
-    surfaceContainerLowest = dark_theme_surface_container_lowest,
-    surfaceContainerLow = dark_theme_surface_container_low,
-    surfaceContainer = dark_theme_surface_container,
-    surfaceContainerHigh = dark_theme_surface_container_high,
-    surfaceContainerHighest = dark_theme_surface_container_highest,
-    inversePrimary = dark_theme_inverse_primary,
-    inverseSurface = dark_theme_inverse_surface,
-    inverseOnSurface = dark_theme_inverse_on_surface,
-    scrim = dark_theme_scrim,
+private val darkScheme = darkColorScheme(
+    primary = primaryDark,
+    onPrimary = onPrimaryDark,
+    primaryContainer = primaryContainerDark,
+    onPrimaryContainer = onPrimaryContainerDark,
+    secondary = secondaryDark,
+    onSecondary = onSecondaryDark,
+    secondaryContainer = secondaryContainerDark,
+    onSecondaryContainer = onSecondaryContainerDark,
+    tertiary = tertiaryDark,
+    onTertiary = onTertiaryDark,
+    tertiaryContainer = tertiaryContainerDark,
+    onTertiaryContainer = onTertiaryContainerDark,
+    error = errorDark,
+    onError = onErrorDark,
+    errorContainer = errorContainerDark,
+    onErrorContainer = onErrorContainerDark,
+    background = backgroundDark,
+    onBackground = onBackgroundDark,
+    surface = surfaceDark,
+    onSurface = onSurfaceDark,
+    surfaceVariant = surfaceVariantDark,
+    onSurfaceVariant = onSurfaceVariantDark,
+    outline = outlineDark,
+    outlineVariant = outlineVariantDark,
+    scrim = scrimDark,
+    inverseSurface = inverseSurfaceDark,
+    inverseOnSurface = inverseOnSurfaceDark,
+    inversePrimary = inversePrimaryDark,
+    surfaceDim = surfaceDimDark,
+    surfaceBright = surfaceBrightDark,
+    surfaceContainerLowest = surfaceContainerLowestDark,
+    surfaceContainerLow = surfaceContainerLowDark,
+    surfaceContainer = surfaceContainerDark,
+    surfaceContainerHigh = surfaceContainerHighDark,
+    surfaceContainerHighest = surfaceContainerHighestDark,
 )
 
 @Composable
@@ -91,7 +91,7 @@ fun KanjiMasteryTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    content: @Composable() () -> Unit
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
@@ -99,8 +99,8 @@ fun KanjiMasteryTheme(
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        darkTheme -> darkScheme
+        else -> lightScheme
     }
 
     MaterialTheme(
--- app/src/main/java/fr/wakuir/kanjimastery/ui/theme/Type.kt
+++ app/src/main/java/fr/wakuir/kanjimastery/ui/theme/Type.kt
@@ -1,13 +1,10 @@
 package fr.wakuir.kanjimastery.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import fr.wakuir.kanjimastery.R
 
 // Common font family
 val fontFamily = FontFamily.Default
